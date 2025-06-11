86f5ec462b70d1413a477972fce4efde62bd7837
Shreeya Chougule <shreeyachougule1111@gmail.com>
2025-06-11 09:18:41 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/about";
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Headers/Header";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import AdmissionsPage from "./pages/AdmissionsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-const App = () => {
+function App() {
   return (
-    <div>
-        <Router>
+    <Router>
+      <div className="main-layout">
+        <Header />
+        <div className="content">
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/about" element={<About />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/ContactPage" element={<ContactPage />} />
+            <Route path="/AboutPage" element={<AboutPage />} />
+            <Route path="/CoursesPage" element={<CoursesPage />} />
+            <Route path="/AdmissionsPage" element={<AdmissionsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
-        </Router>
-    </div>
-  )
-}
+        </div>
 
-export default App
+      </div>
+    </Router>
+  );
+}
+export default App;
+
